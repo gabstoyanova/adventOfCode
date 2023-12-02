ca2753c84c838b175c1f16f97a9c235d6060fc68
Gabriela Stoyanova <gabstoyanova1@gmail.com>
2023-12-02 21:04:29 +0200

    day 1 part 2

--- src/adventOfCode2023/days/day1/Day1.java
+++ src/adventOfCode2023/days/day1/Day1.java
@@ -2,10 +2,28 @@ package adventOfCode2023.days.day1;
 
 import adventOfCode2023.Day;
 
+import java.util.List;
+
 public class Day1 extends Day {
+
+  private static final List<String> numbers = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+
   public static void main(String[] args) {
     Day1 day1 = new Day1();
     System.out.println(day1.part1());
+    System.out.println(day1.part2());
+  }
+
+  @Override
+  public Object part1() {
+    var input = readInputFile("src/adventOfCode2023/days/day1/input");
+    return "Part 1: " + input.mapToInt(Day1::calculateLineNumber).sum();
+  }
+
+  @Override
+  public Object part2() {
+    var input = readInputFile("src/adventOfCode2023/days/day1/input");
+    return "Part 2: " + input.map(Day1::convertLineToNumbers).mapToInt(Day1::calculateLineNumber).sum();
   }
 
   public static int calculateLineNumber(String line) {
@@ -26,16 +44,13 @@ public class Day1 extends Day {
     return Integer.parseInt(first + "" + last);
   }
 
-  @Override
-  public Object part1() {
-    var input = readInputFile("src/adventOfCode2023/days/day1/input");
-
-    return "Part 1: " + input.mapToInt(Day1::calculateLineNumber).sum();
+  private static String convertLineToNumbers(String line) {
+    for (int i = 0; i < numbers.size(); i++) {
+      var number = numbers.get(i);
+      while (line.contains(number)) {
+        line = line.replaceFirst(number, "" + number.charAt(0) + (i + 1) + number.charAt(number.length() - 1));
+      }
+    }
+    return line;
   }
-
-  @Override
-  public Object part2() {
-    return null;
-  }
-
 }
